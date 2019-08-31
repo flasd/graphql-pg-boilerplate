7bec53feac11916fd0d9f7290e31fce81b00b7ff
Marcel Coelho <flasd2@gmail.com>
2019-08-31 16:58:59 -0300

    fix: replace template ids with env ids

---
+++ src/services/email/index.js
@@ -0,0 +1,30 @@
+const sendgrid = require('@sendgrid/mail');
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+const EMAILS = {
+  EMAIL_VERIFICATION: {
+    templateId: process.env.EMAIL_CONFIRMATION_TEMPLATE_ID,
+  },
+
+  PASSWORD_RECOVERY: {
+    templateId: process.env.PASSWORD_RECOVERY_TEMPLATE_ID,
+  },
+};
+
+async function send(email, to, variables) {
+  try {
+    await sendgrid.send({
+      to,
+      from: process.env.EMAIL_FROM,
+      templateId: email.templateId,
+      dynamic_template_data: variables,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+send.EMAILS = EMAILS;
+
+module.exports = send;

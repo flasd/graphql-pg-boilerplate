5cca2e7aa4ce32e98d61d19ebc155d6ca5543042
Marcel Coelho <flasd2@gmail.com>
2019-09-30 17:46:42 -0300

    feat: add race condition detection to prevent double eslint output

--- scripts/on-nodemon-restart.js
+++ scripts/on-nodemon-restart.js
@@ -2,14 +2,27 @@
 require('dotenv').config();
 // eslint-disable-next-line import/no-extraneous-dependencies
 const shellExec = require('shell-exec');
+const fs = require('fs');
+const path = require('path');
+
+const lastProcess = path.join(__dirname, '.last.txt');
 
 async function main() {
+  const id = Math.round(Math.random() * 10000);
+  fs.writeFileSync(lastProcess, id);
+
   const isWindows = process.platform === 'win32';
   const CLEAR = isWindows ? '\x1Bc' : '\x1B[2J\x1B[3J\x1B[H';
 
   process.stdout.write(CLEAR);
 
   const result = await shellExec('yarn lint --color');
+
+  const latestProcessId = fs.readFileSync(lastProcess).toString();
+
+  if (parseInt(latestProcessId, 10) !== id) {
+    return;
+  }
 
   const parsed = result.stdout.replace(/^.+\n/, '');
 
